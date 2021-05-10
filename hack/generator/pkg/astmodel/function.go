e0dfd9898e68998061e4c75a5e1474e0faa968ad
Matthew Christopher <matthchr@users.noreply.github.com>
2021-05-10 11:50:41 -0700

    Make validator and defaulter better

--- hack/generator/pkg/astmodel/function.go
+++ hack/generator/pkg/astmodel/function.go
@@ -27,3 +27,85 @@ type Function interface {
 	// Equals determines if this Function is equal to another one
 	Equals(f Function) bool
 }
+
+var _ Function = &objectFunction{}
+
+type objectFunctionHandler func(f *objectFunction, codeGenerationContext *CodeGenerationContext, receiver TypeName, methodName string) *dst.FuncDecl
+
+// objectFunction is a simple helper that implements the Function interface. It is intended for use for functions
+// that only need information about the object they are operating on
+type objectFunction struct {
+	name             string
+	o                *ObjectType
+	idFactory        IdentifierFactory
+	asFunc           objectFunctionHandler
+	requiredPackages *PackageReferenceSet
+}
+
+// Name returns the unique name of this function
+// (You can't have two functions with the same name on the same object or resource)
+func (k *objectFunction) Name() string {
+	return k.name
+}
+
+func (k *objectFunction) RequiredPackageReferences() *PackageReferenceSet {
+	return k.requiredPackages
+}
+
+func (k *objectFunction) References() TypeNameSet {
+	return k.o.References()
+}
+
+func (k *objectFunction) AsFunc(codeGenerationContext *CodeGenerationContext, receiver TypeName) *dst.FuncDecl {
+	return k.asFunc(k, codeGenerationContext, receiver, k.name)
+}
+
+func (k *objectFunction) Equals(f Function) bool {
+	typedF, ok := f.(*objectFunction)
+	if !ok {
+		return false
+	}
+
+	// TODO: We're not actually checking function structure here
+	return k.o.Equals(typedF.o) && k.name == typedF.name
+}
+
+type resourceFunctionHandler func(f *resourceFunction, codeGenerationContext *CodeGenerationContext, receiver TypeName, methodName string) *dst.FuncDecl
+
+// resourceFunction is a simple helper that implements the Function interface. It is intended for use for functions
+// that only need information about the resource they are operating on
+type resourceFunction struct {
+	name             string
+	resource         *ResourceType
+	idFactory        IdentifierFactory
+	asFunc           resourceFunctionHandler
+	requiredPackages *PackageReferenceSet
+}
+
+// Name returns the unique name of this function
+// (You can't have two functions with the same name on the same object or resource)
+func (r *resourceFunction) Name() string {
+	return r.name
+}
+
+func (r *resourceFunction) RequiredPackageReferences() *PackageReferenceSet {
+	return r.requiredPackages
+}
+
+func (r *resourceFunction) References() TypeNameSet {
+	return r.resource.References()
+}
+
+func (r *resourceFunction) AsFunc(codeGenerationContext *CodeGenerationContext, receiver TypeName) *dst.FuncDecl {
+	return r.asFunc(r, codeGenerationContext, receiver, r.name)
+}
+
+func (r *resourceFunction) Equals(f Function) bool {
+	typedF, ok := f.(*resourceFunction)
+	if !ok {
+		return false
+	}
+
+	// TODO: We're not actually checking function structure here
+	return r.resource.Equals(typedF.resource) && r.name == typedF.name
+}
